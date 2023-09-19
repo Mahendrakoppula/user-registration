a73641fb5be95d5f43deca0b3bf4712efa6a94ee
simpleneeraj <simplneeraj@gmail.com>
2023-09-20 05:29:33 +0530

    ui fix

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,41 +1,49 @@
-import React from "react";
+import Input from '@/components/input';
+import React from 'react';
 
 const PaymentPage = () => {
-  return     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <figure className="flex flex-col items-center max-w-lg">
+        <img
+          className="rounded-lg h-2/4 w-2/4"
+          src="https://cdn.britannica.com/17/155017-050-9AC96FC8/Example-QR-code.jpg"
+          alt="image description"
+        />
+        <figcaption className="mt-2 text-base text-center text-gray-500 dark:text-gray-400">
+          Scan the QR Code for payment
+        </figcaption>
+      </figure>
 
-    <div>
-        <img  src="https://cdn.britannica.com/17/155017-050-9AC96FC8/Example-QR-code.jpg" width={200} height={200} alt="" />
-        <p>scan the qr code for payment</p> 
-        
-    </div>
-    <div>
-        <div>
-            <p>Upload the screenshot </p>
+      <div className="inline-flex items-center justify-center w-full">
+        <hr className="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+        <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+          or
+        </span>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <Input
+          label="Upload Screenshot of Payment"
+          id="user_avatar"
+          type="file"
+        />
+        <div
+          className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+          id="user_avatar_help"
+        >
+          PNG, JPG, GIF up to 10MB
         </div>
-    <div className="mb-6">
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            10th certificate
-          </label>
-          <input
-            type="file"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="john.doe@company.com"
-            required
-          />
-        </div>
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
-    
-    </div>
-</main>
+      </div>
+    </main>
+  );
 };
 
 export default PaymentPage;
---
+++ components/wraper.tsx
@@ -0,0 +1,23 @@
+type InputFieldProps = {
+  id?: string;
+  label: string;
+  children: React.ReactNode | React.ReactNode[];
+  className?: string;
+};
+
+const Wraper = (props: InputFieldProps) => {
+  return (
+    <div className={`${props.className} flex flex-col`}>
+      <label
+        htmlFor={props.id}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {props.label}
+      </label>
+
+      {props.children}
+    </div>
+  );
+};
+
+export default Wraper;
